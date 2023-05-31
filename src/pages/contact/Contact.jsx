57ac38aa7c23165bf39a9731e527bbcc94d452a8
Janammjay <janammjayk6@gmail.com>
2023-05-31 18:04:18 +0530

    completed

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -3,8 +3,9 @@ import emailjs from "emailjs-com";
 import con from "./contact.module.css";
 import { Animate } from "react-simple-animate";
 import { BsInfoCircleFill } from "react-icons/bs";
-import galaxy from "./images/galaxy.png";
 import swal from "sweetalert";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 
 const Contact = () => {
   const [formValues, setFormValues] = useState({
@@ -80,16 +81,13 @@ const Contact = () => {
     return true;
   };
 
+  const particlesInit = async (main) => {
+    await loadFull(main);
+  };
+
   return (
     <form onSubmit={handleSubmit}>
-      <div
-        className={con.content}
-        style={{
-          background: `url(${galaxy})`,
-          backgroundPosition: "center",
-          backgroundSize: "cover",
-        }}
-      >
+      <div className={con.content}>
         <Animate
           play
           duration={1}
@@ -167,6 +165,127 @@ const Contact = () => {
             </button>
           </div>
         </Animate>
+      </div>
+      <div className={con.particle}>
+        <Particles
+          id="tsparticles"
+          init={particlesInit}
+          options={{
+            fullScreen: {
+              enable: true,
+              zIndex: -1,
+            },
+            particles: {
+              number: {
+                value: 160,
+                density: {
+                  enable: true,
+                  value_area: 800,
+                },
+              },
+              color: {
+                value: "#daa520",
+              },
+              shape: {
+                type: "circle",
+                stroke: {
+                  width: 0,
+                  color: "goldenrod",
+                },
+                polygon: {
+                  nb_sides: 10,
+                },
+              },
+              opacity: {
+                value: 1,
+                random: true,
+                anim: {
+                  enable: true,
+                  speed: 1,
+                  opacity_min: 0,
+                  sync: false,
+                },
+              },
+              size: {
+                value: 4,
+                random: true,
+                anim: {
+                  enable: false,
+                  speed: 102.09290454313434,
+                  size_min: 0.3,
+                  sync: false,
+                },
+              },
+              line_linked: {
+                enable: false,
+                distance: 150,
+                color: "#ffffff",
+                opacity: 0.4,
+                width: 1,
+              },
+              move: {
+                enable: true,
+                speed: 1,
+                direction: "none",
+                random: true,
+                straight: false,
+                out_mode: "out",
+                bounce: false,
+                attract: {
+                  enable: false,
+                  rotateX: 600,
+                  rotateY: 600,
+                },
+              },
+            },
+            interactivity: {
+              detect_on: "canvas",
+              events: {
+                onhover: {
+                  enable: true,
+                  mode: "bubble",
+                },
+                onclick: {
+                  enable: true,
+                  mode: "repulse",
+                },
+                resize: true,
+              },
+              modes: {
+                grab: {
+                  distance: 400,
+                  line_linked: {
+                    opacity: 1,
+                  },
+                },
+                bubble: {
+                  distance: 250,
+                  size: 0,
+                  duration: 2,
+                  opacity: 0,
+                  speed: 3,
+                },
+                repulse: {
+                  distance: 400,
+                  duration: 0.4,
+                },
+                push: {
+                  particles_nb: 4,
+                },
+                remove: {
+                  particles_nb: 2,
+                },
+              },
+            },
+            retina_detect: true,
+            background: {
+              color: "#111",
+              position: "50% 50%",
+              repeat: "no-repeat",
+              size: "cover",
+            },
+          }}
+        />
       </div>
     </form>
   );
